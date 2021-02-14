2006f5d17d57147a1d837de3ce317cfd0aa20011
RJ <32805187+ryanjonesrocks@users.noreply.github.com>
2021-02-14 16:03:40 -0700

    Update masscan_output_updated.py

--- masscan_output_updated.py
+++ masscan_output_updated.py
@@ -8,14 +8,14 @@ for line in Lines:
     # For every port try both HTTP and HTTPS
     
     #HTTP
-    if temp_split[1] == "443" || temp_split[1] == "80":
+    if temp_split[1] == "443" or temp_split[1] == "80":
     	http_formatted_line = ("http://" + temp_split[0])
     else:
         http_formatted_line = ("http://" + temp_split[0] + ":" + temp_split[1])
 
 
     #HTTPS
-    if temp_split[1] == "443" || temp_split[1] == "80":
+    if temp_split[1] == "443" or temp_split[1] == "80":
     	https_formatted_line = ("https://" + temp_split[0])
     else:
         https_formatted_line = ("https://" + temp_split[0] + ":" + temp_split[1])
